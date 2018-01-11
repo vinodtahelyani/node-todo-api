de9730fffa6804e6632bf4f93b5ede35c10d6c4e
Vinod Tahelyani <vinod.tahelynai2016@vitstudent.ac.in>
2018-01-11 23:28:42 +0530

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,30 @@
+var {MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://test:test@ds247027.mlab.com:47027/todo-api',(err,db)=>{
+    if(err){
+        return console.log('unable to connect');
+    }
+    console.log('success fully connected');
+//     db.collection('user').find().toArray().then((docs)=>{
+//         console.log(JSON.stringify(docs,undefined,2));
+//     }).catch((err)=>{
+//         console.log(err.message);
+//     })
+// 
+
+
+        // db.collection('user').deleteMany({name:'vinod'}).then((result)=>{
+        //     console.log(result);
+        // }).catch((err)=>{
+        //     console.log(err.message);
+        // });
+
+        db.collection('user').findOneAndDelete({name:'vinod thaelyani'}).then((result)=>{
+            console.log(JSON.stringify(result,undefined,2));
+        }).catch((err)=>{
+            console.log(err.message);
+        });
+
+
+
+});
