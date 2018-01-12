9cb09f511430be33c85b250b55357e3e812e5060
Vinod Tahelyani <vinod.tahelynai2016@vitstudent.ac.in>
2018-01-13 00:03:37 +0530

    commit

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -19,10 +19,37 @@ MongoClient.connect('mongodb://test:test@ds247027.mlab.com:47027/todo-api',(err,
         //     console.log(err.message);
         // });
 
-        db.collection('user').findOneAndDelete({name:'vinod thaelyani'}).then((result)=>{
-            console.log(JSON.stringify(result,undefined,2));
-        }).catch((err)=>{
-            console.log(err.message);
+        // db.collection('user').findOneAndDelete({name:'vinod thaelyani'}).then((result)=>{
+        //     console.log(JSON.stringify(result,undefined,2));
+        // }).catch((err)=>{
+        //     console.log(err.message);
+        // });
+
+        // db.collection('user').findOneAndUpdate({
+        //     //_id: new ObjectID('5a5640f275475d153befc64e')
+        //     name:name+"jhony"
+        // },{
+        //     $set:{
+        //         location: 'delhi'
+        //     }
+        // },{
+        //         returnOriginal:false
+        //     }).then((result)=>{
+        //         console.log(result);
+        //     }).catch((err)=>{
+        //         console.log(err.message);
+        //     });
+
+        db.collection('user').findOneAndUpdate({
+            name:"jhony bravo"
+        },{
+            $inc:{
+                age:1
+            }
+        },{
+            returnOriginal:false
+        }).then((doc)=>{
+            console.log(doc);
         });
 
 
